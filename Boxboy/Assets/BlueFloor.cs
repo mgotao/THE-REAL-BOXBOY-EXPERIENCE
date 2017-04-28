db5c415b559217a730578b84209b0addb31723ac
mgotao <m.gotao@yahoo.com>
2017-04-28 16:08:44 -0700

    Updated character movement scripts and levels

---
+++ Boxboy/Assets/BlueFloor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class BlueFloor : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider dood)
+    {
+        if (dood.tag == "Player")
+        {
+            player.GetComponent<RigidbodyFirstPersonController>().movementSettings.ForwardSpeed += 30;
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        player.GetComponent<RigidbodyFirstPersonController>().movementSettings.ForwardSpeed -= 30;
+    }
+}
---
+++ Boxboy/Assets/OrangeFloor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class OrangeFloor : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider dood)
+    {
+        if (dood.tag == "Player")
+        {
+            player.GetComponent<Rigidbody>().mass -= 2;
+            player.GetComponent<RigidbodyFirstPersonController>().movementSettings.JumpForce *= 2;
+            player.GetComponent<RigidbodyFirstPersonController>().m_Jump = true;
+            
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        player.GetComponent<Rigidbody>().mass += 2;
+        player.GetComponent<RigidbodyFirstPersonController>().movementSettings.JumpForce /= 2;
+    }
+}
