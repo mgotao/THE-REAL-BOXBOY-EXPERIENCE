54b3a27a0c1057c96b405c921474104eb23d0349
mgotao <m.gotao@yahoo.com>
2017-05-10 22:59:52 -0700

    Add files via upload

---
+++ Boxboy/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FastFloor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FastFloor : MonoBehaviour {
+    public GameObject player;
+    float checkSpeed, checkMass;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        checkMass = player.GetComponent<Rigidbody>().mass;
+        checkSpeed = player.GetComponent<RigidbodyFirstPersonController>().movementSettings.ForwardSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider dood)
+    {
+        if (dood.tag == "Player")
+        {
+            if (checkMass >= 10)
+            {
+                checkMass -= 2;
+                player.GetComponent<Rigidbody>().mass = checkMass;
+            }
+            if (checkSpeed <= 6)
+            {
+                checkSpeed += 12;
+                player.GetComponent<RigidbodyFirstPersonController>().movementSettings.ForwardSpeed = checkSpeed;
+            }
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        if (checkMass <= 8)
+        {
+            checkMass += 2;
+            player.GetComponent<Rigidbody>().mass = checkMass;
+        }
+        if (checkSpeed >= 18)
+        {
+            checkSpeed -= 12;
+            player.GetComponent<RigidbodyFirstPersonController>().movementSettings.ForwardSpeed = checkSpeed;
+        }
+    }
+}
---
+++ Boxboy/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/JumpFloor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class JumpFloor : MonoBehaviour {
+    public GameObject player;
+    float checkJump, checkMass;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        checkMass = player.GetComponent<Rigidbody>().mass;
+        checkJump = player.GetComponent<RigidbodyFirstPersonController>().movementSettings.JumpForce;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider dood)
+    {
+        if (dood.tag == "Player")
+        {
+            if (checkMass >= 10)
+            {
+                checkMass -= 2;
+                player.GetComponent<Rigidbody>().mass = checkMass;
+            }
+            if (checkJump <= 80)
+            {
+                checkJump += 20;
+                player.GetComponent<RigidbodyFirstPersonController>().movementSettings.JumpForce = checkJump;
+            }
+            player.GetComponent<RigidbodyFirstPersonController>().m_Jump = true;
+            
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        if (checkMass <= 8)
+        {
+            checkMass += 2;
+            player.GetComponent<Rigidbody>().mass = checkMass;
+        }
+        if (checkJump >= 100)
+        {
+            checkJump -= 20;
+            player.GetComponent<RigidbodyFirstPersonController>().movementSettings.JumpForce = checkJump;
+        }
+    }
+}
